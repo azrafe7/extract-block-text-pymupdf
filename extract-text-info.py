af6a2c8be91fe1b32f91d9eb48db57e3cf804bbc
Giuseppe Di Mauro <azrafe7@gmail.com>
2024-08-13 19:47:07 +0200

    add cluster_blocks and other features

--- extract-text-info.py
+++ extract-text-info.py
@@ -8,6 +8,115 @@ import PIL
 
 HIGHLIGHTED_SUFFIX = '_highlighted'
 
+# get_text params
+DEFAULT_FLAGS = 0
+DEFAULT_SORT = True
+
+# clustered blocks params
+DEFAULT_USE_CLUSTERED_BLOCKS = True
+DEFAULT_X_TOLERANCE = 0
+DEFAULT_Y_TOLERANCE = 3
+
+# Adapted from cluster_drawings()
+def cluster_blocks(
+    page, clip=None, blocks=None, x_tolerance: float = 3, y_tolerance: float = 3
+) -> list:
+    """Join rectangles of neighboring vector graphic items.
+
+    Args:
+        clip: optional rect-like to restrict the page area to consider.
+        blocks: (optional) output of a previous "get_drawings()".
+        x_tolerance: horizontal neighborhood threshold.
+        y_tolerance: vertical neighborhood threshold.
+
+    Notes:
+        Vector graphics (also called line-art or blocks) usually consist
+        of independent items like rectangles, lines or curves to jointly
+        form table grid lines or bar, line, pie charts and similar.
+        This method identifies rectangles wrapping these disparate items.
+
+    Returns:
+        A list of Rect items, each wrapping line-art items that are close
+        enough to be considered forming a common vector graphic.
+        Only "significant" rectangles will be returned, i.e. having both,
+        width and height larger than the tolerance values.
+    """
+    parea = page.rect  # the default clipping area
+    if clip is not None:
+        parea = Rect(clip)
+    delta_x = x_tolerance  # shorter local name
+    delta_y = y_tolerance  # shorter local name
+    if blocks is None:  # if we cannot re-use a previous output
+        blocks = page.get_text('dict', flags=DEFAULT_FLAGS, sort=DEFAULT_SORT)['blocks']
+
+    def are_neighbors(r1, r2):
+        """Detect whether r1, r2 are "neighbors".
+
+        Items r1, r2 are called neighbors if the minimum distance between
+        their points is less-equal delta.
+
+        Both parameters must be (potentially invalid) rectangles.
+        """
+        # normalize rectangles as needed
+        rr1_x0, rr1_x1 = (r1.x0, r1.x1) if r1.x1 > r1.x0 else (r1.x1, r1.x0)
+        rr1_y0, rr1_y1 = (r1.y0, r1.y1) if r1.y1 > r1.y0 else (r1.y1, r1.y0)
+        rr2_x0, rr2_x1 = (r2.x0, r2.x1) if r2.x1 > r2.x0 else (r2.x1, r2.x0)
+        rr2_y0, rr2_y1 = (r2.y0, r2.y1) if r2.y1 > r2.y0 else (r2.y1, r2.y0)
+        if (
+            0
+            or rr1_x1 < rr2_x0 - delta_x
+            or rr1_x0 > rr2_x1 + delta_x
+            or rr1_y1 < rr2_y0 - delta_y
+            or rr1_y0 > rr2_y1 + delta_y
+        ):
+            # Rects do not overlap.
+            return False
+        else:
+            # Rects overlap.
+            return True
+
+    # add bbox converted to rect
+    for b in blocks: 
+        bbox = b['bbox']
+        b['rect'] = fitz.Rect(x0=bbox[0], y0=bbox[1], x1=bbox[2], y1=bbox[3])
+    
+    # exclude graphics not contained in the clip
+    paths = [
+        p
+        for p in blocks
+        if 1
+        and p["rect"].x0 >= parea.x0
+        and p["rect"].x1 <= parea.x1
+        and p["rect"].y0 >= parea.y0
+        and p["rect"].y1 <= parea.y1
+    ]
+
+    # list of all vector graphic rectangles
+    prects = sorted([p["rect"] for p in paths], key=lambda r: (r.y1, r.x0))
+
+    new_rects = []  # the final list of the joined rectangles
+
+    # -------------------------------------------------------------------------
+    # The strategy is to identify and join all rects that are neighbors
+    # -------------------------------------------------------------------------
+    while prects:  # the algorithm will empty this list
+        r = +prects[0]  # copy of first rectangle
+        repeat = True
+        while repeat:
+            repeat = False
+            for i in range(len(prects) - 1, 0, -1):  # from back to front
+                if are_neighbors(prects[i], r):
+                    r |= prects[i].tl  # include in first rect
+                    r |= prects[i].br  # include in first rect
+                    del prects[i]  # delete this rect
+                    repeat = True
+
+        new_rects.append(r)
+        del prects[0]
+        prects = sorted(set(prects), key=lambda r: (r.y1, r.x0))
+
+    new_rects = sorted(set(new_rects), key=lambda r: (r.y1, r.x0))
+    return [r for r in new_rects if r.width > delta_x and r.height > delta_y]
 
 def get_texts_in_block(block, as_spans=False):
     texts = []
@@ -40,7 +149,7 @@ def flags_decomposer(flags):
         l.append("bold")
     return " ".join(l)
 
-def highlight_sentences_in_pdf(input_pdf_path, output_pdf_path):
+def highlight_sentences_in_pdf(input_pdf_path, output_pdf_path, use_clustered_blocks=DEFAULT_USE_CLUSTERED_BLOCKS, x_tolerance=DEFAULT_X_TOLERANCE, y_tolerance=DEFAULT_Y_TOLERANCE):
 
     # Open the PDF file
     pdf_document = fitz.open(input_pdf_path)
@@ -110,12 +219,43 @@ def highlight_sentences_in_pdf(input_pdf_path, output_pdf_path):
             #    imgout.close()
         
             # for k in range(len(bbox)): bbox[k] += 2 if k < 2 else -2  # shrink bbox
-            highlight = page.add_rect_annot(bbox)
-            highlight.set_colors(stroke=[0, .2, 1])  # Blue rectangle
-            highlight.update()
+            #highlight = page.add_rect_annot(bbox)
+            #highlight.set_colors(stroke=[0, .2, 1])  # Blue rectangle
+            #highlight.update()
 
         # Extract text with formatting information
-        blocks = page.get_text("dict")["blocks"]
+        all_text_blocks = [block for block in page.get_text("dict", flags=DEFAULT_FLAGS, sort=DEFAULT_SORT)["blocks"] if block['type'] == 0]
+                
+        if use_clustered_blocks:
+            page_data["use_clustered_blocks"] = {
+                "x_tolerance": x_tolerance,
+                "y_tolerance": y_tolerance,
+            }
+            clustered_rects = cluster_blocks(page, blocks=all_text_blocks, x_tolerance=x_tolerance, y_tolerance=y_tolerance)
+            blocks = []
+            print(f"all_text_blocks: {len(all_text_blocks)}  clustered_rects: {len(clustered_rects)}")
+            for rect in clustered_rects:
+                merged_blocks = [clipped_block for clipped_block in page.get_text("dict", clip=rect, flags=DEFAULT_FLAGS, sort=DEFAULT_SORT)["blocks"] if clipped_block['type'] == 0]
+                # breakpoint()
+                if len(merged_blocks) >= 1:
+                    merged_block = merged_blocks[0]
+                    for other_block in merged_blocks[1:]:
+                        merged_block['lines'] += other_block['lines']
+                        # breakpoint()
+                        merged_rect = fitz.Rect(merged_block['bbox']) | fitz.Rect(other_block['bbox'])
+                        merged_block['bbox'] = [merged_rect.x0, merged_rect.y0, merged_rect.x1, merged_rect.y1]
+                    blocks.append(merged_block)
+        else:
+            page_data["use_clustered_blocks"] = False
+            blocks = all_text_blocks
+        
+        print(f"blocks: {len(blocks)}")
+        # breakpoint()
+        
+        # output.json
+        #json_output = json.loads(page.get_text("json", sort=True))
+        #with open('output.json', 'w') as f:
+        #    json.dump(json_output, f, indent=2)
         
         for block_index, block in enumerate(blocks):
             if block['type'] == 0:  # Text block
@@ -128,13 +268,14 @@ def highlight_sentences_in_pdf(input_pdf_path, output_pdf_path):
                     text = ''.join([span['text'] for span in spans])
                     print(f"[page {page_number}] spans text in block {block_index} line {line_index}: '{text}'")
                     bbox = line['bbox']
-                    highlight = page.add_rect_annot(bbox)
-                    highlight.set_colors(stroke=[1, 0, 0])  # Red rectangle
-                    highlight.update()
-                    # Add annotation with line text
-                    text_annot = page.add_text_annot((bbox[2]-2, bbox[3]-2), text, icon="Comment")
-                    text_annot.set_colors(stroke=[1, 0, 0])  # Red
-                    text_annot.update(opacity=.7)
+                    
+                    #highlight = page.add_rect_annot(bbox)
+                    #highlight.set_colors(stroke=[1, 0, 0])  # Red rectangle
+                    #highlight.update()
+                    ## Add annotation with line text
+                    #text_annot = page.add_text_annot((bbox[2]-2, bbox[3]-2), text, icon="Comment")
+                    #text_annot.set_colors(stroke=[1, 0, 0])  # Red
+                    #text_annot.update(opacity=.7)
                     
                     for span in spans:
                         # breakpoint()
@@ -200,6 +341,7 @@ def main():
     # Highlight the sentences in the PDF
     json_data = highlight_sentences_in_pdf(input_pdf, output_pdf)
     print()
+    print(f"use_clustered_blocks: {json_data[-1]['use_clustered_blocks']}")
     print(f"Highlighted PDF saved as: {output_pdf}")
     
     with open(output_json, 'w', encoding='utf-8') as f:
